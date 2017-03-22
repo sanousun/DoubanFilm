dad6412dfffb3daa3966b2cb7b603e48449a33fe
大树 <dashu@maihaoche.com>
2017-03-22 14:25:18 +0800

    依赖库更改，准备开发2.0

---
+++ app/src/main/java/com/shenhui/doubanfilm/data/LoadService.java
@@ -0,0 +1,13 @@
+package com.shenhui.doubanfilm.data;
+
+/**
+ * Created with Android Studio.
+ * User: dashu
+ * Date: 2017/3/22
+ * Time: 下午2:23
+ * Desc: retrofit的接口定义
+ */
+
+public interface LoadService {
+
+}
